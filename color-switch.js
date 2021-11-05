9918b364036c698f7805b5f2c8b9b96ebe8b915b
Nataliia Kotelnitska <nkotelnicka@gmail.com>
2021-11-06 00:41:18 +0200

    update color-swich.js

---
+++ color-switch.js
@@ -0,0 +1,25 @@
+const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'];
+const refs = {
+  body: document.body,
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+};
+
+refs.startBtn.addEventListener('click', onRandomBodyColor);
+refs.stopBtn.addEventListener('click', onStopBtn);
+
+function onRandomBodyColor(evt) {
+  intervalID = setInterval(() => {
+    refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  }, 1000);
+  refs.startBtn.disabled = true;
+}
+
+function onStopBtn() {
+  clearInterval(intervalID);
+  refs.startBtn.disabled = false;
+}
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
